ad5bcd2971a3403b6c44e65772d69ceb2c5c9922
claire <kclaire1522@gmail.com>
2025-02-02 21:32:41 +0900

    Added Game Over UI

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
+    public GameObject gameOverScreen;
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
@@ -14,17 +16,14 @@ public class LogicScript : MonoBehaviour
         playerScore = playerScore + scoreToAdd;
         scoreText.text = playerScore.ToString();
     }
-
-
-    // Start is called before the first frame update
-    void Start()
+    
+    public void restartGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void gameOver()
     {
-        
+        gameOverScreen.SetActive(true);
     }
 }
